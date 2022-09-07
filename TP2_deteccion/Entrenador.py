759f0523a7d4103f7930aa320159e535f06ee893
AgustinaVerschoor <agustina.verschoor@ing.austral.edu.ar>
2022-09-07 19:20:52 -0300

    el entrenador anda

--- TP2_deteccion/Entrenador.py
+++ TP2_deteccion/Entrenador.py
@@ -1,25 +1,39 @@
-from sklearn import tree
-from joblib import dump, load
-from Generador_de_descriptores import library, labels, to_gray, to_binary, find_contours, getBiggerContour, final_contoursArray, hu_moments
+from sklearn import *
+from joblib import dump
+import xlwings as xw
+from sklearn.model_selection import train_test_split # Import train_test_split function
 
 # dataset
+ws = xw.Book("data.xlsx").sheets['sheet1']
+y = ws.range("A2:A31").value
+hu1 = ws.range("B2:B31").value
+hu2 = ws.range("C2:C31").value
+hu3 = ws.range("D2:D31").value
+hu4 = ws.range("E2:E31").value
+hu5 = ws.range("F2:F31").value
+hu6 = ws.range("G2:G31").value
+hu7 = ws.range("H2:H31").value
+x = [
+    [float(hu1[i]),
+     float(hu2[i]),
+     float(hu3[i]),
+     float(hu4[i]),
+     float(hu5[i]),
+     float(hu6[i]),
+     float(hu7[i])]
+    for i in range(30)
+]
 
-
-X = get_dataset(library(), 30)
-
-Y = labels()
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.3, random_state=1) # 70% training and 30% test
 
 # entrenamiento
-clasificador = tree.DecisionTreeClassifier().fit(X, Y)
-
+clasificador = tree.DecisionTreeClassifier().fit(x_train, y_train)
+y_pred=clasificador.predict(x_test)
 # visualización del árbol de decisión resultante
-tree.plot_tree(clasificador)
+_=tree.plot_tree(clasificador)
 
 # guarda el modelo en un archivo
 dump(clasificador, 'filename.joblib')
-
-# en otro programa, se puede cargar el modelo guardado
-clasificadorRecuperado = load('filename.joblib')
 
 
 
